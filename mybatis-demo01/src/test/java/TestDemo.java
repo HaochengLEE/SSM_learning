dcd18b12667cab57dbcf20aa36f2fe2e04ffc423
lihaocheng <lihaocheng00@126.com>
2018-10-28 00:49:14 +0800

    test01

--- mybatis-demo01/src/test/java/TestDemo.java
+++ mybatis-demo01/src/test/java/TestDemo.java
@@ -1,3 +1,4 @@
+import com.dome.entity.Users;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -5,17 +6,40 @@ import org.junit.Test;
 
 import javax.annotation.Resources;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * @author lihaocheng
  * @create 2018-10-26 下午11:27
  **/
 
-public class TestDemo {
-    //cscs
-    String resource = "mybatis.xml";
-    InputStream is = Resources.class.getResourceAsStream(resource);
-    SqlSession factory = new SqlSessionFactoryBuilder().build(is);
+public class TestDemo{
+    @Test
+    public void testDemo() throws Exception{
+        //初始化mybatis配置环境
+        String resource = "mybatis.xml";
+        InputStream is = org.apache.ibatis.io.Resources.getResourceAsStream(resource);
+//        if(is==null){
+//            System.out.println("is");
+//        }
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+
+        //打开和数据库之间的对话
+        SqlSession session = factory.openSession();
+        if(session==null){
+            System.out.println("数据库未连接");
+        }
+
+        List<Users> ulist=session.selectList("usersList");
+
+        for(Users user:ulist){
+            System.out.println(user);
+        }
+        //关闭资源
+        session.close();
+    }
+
+
 
 
 
