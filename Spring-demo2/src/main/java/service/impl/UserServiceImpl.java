48912e4fe377e378d80f01a63c62f4eb46203927
lihaocheng <lihaocheng00@126.com>
2018-11-05 23:58:12 +0800

    aopinit

---
+++ Spring-demo2/src/main/java/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package service.impl;
+
+import service.UserService;
+import dao.UserDao;
+import entity.User;
+
+/**
+ * 用户业务类，实现对User功能的业务管理
+ */
+public class UserServiceImpl implements UserService {
+
+	// 声明接口类型的引用，和具体实现类解耦合
+	private UserDao dao;
+
+	// dao 属性的setter访问器，会被Spring调用，实现设值注入
+	public void setDao(UserDao dao) {
+		this.dao = dao;
+	}
+
+	public void addNewUser(User user) {
+		// 调用用户DAO的方法保存用户信息
+		dao.save(user);
+	}
+}
